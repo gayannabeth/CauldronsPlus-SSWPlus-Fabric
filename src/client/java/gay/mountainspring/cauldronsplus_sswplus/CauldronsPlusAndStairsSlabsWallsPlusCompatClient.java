b9554a766d2afe1f5dd3626009ff2c370b147749
gayannabeth <annabethbruce379@gmail.com>
2026-02-05 16:16:06 -0500

    Initial Commit

---
+++ src/client/java/gay/mountainspring/cauldronsplus_sswplus/CauldronsPlusAndStairsSlabsWallsPlusCompatClient.java
@@ -0,0 +1,10 @@
+package gay.mountainspring.cauldronsplus_sswplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CauldronsPlusAndStairsSlabsWallsPlusCompatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
